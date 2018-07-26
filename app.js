02d02422d6d88e94a3facde330d10d6051432878
Gavin Mogan <git@gavinmogan.com>
2018-07-25 23:56:38 -0700

    no favicon for us, its either on s3 or nowhere

--- app.js
+++ app.js
@@ -8,7 +8,6 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
-// var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var AWS = require('aws-sdk');
@@ -51,8 +50,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('trust proxy', 1); // trust first proxy
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
